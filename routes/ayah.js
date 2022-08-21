132eef314f22946f370b3f1111aefe7464891cd0
haqnawaz <haqnawaz99@yahoo.com>
2022-08-21 15:54:07 +0500

    haqnawaz changes

--- routes/ayah.js
+++ routes/ayah.js
@@ -1,9 +1,10 @@
-const express = require('express');
-const router = express.Router();
-const { ayahs, ayahById ,getAyahsBySurah} = require('../controllers/ayahs');
+const express = require("express");
 
-router.route('/all').get(ayahs);
-router.route('/:id').get(ayahById);
-router.route('/get-ayahs-by-surah/:id').get(getAyahsBySurah);
+const router = express.Router();
+const { ayahs, ayahById, getAyahsBySurah } = require("../controllers/ayahs");
+
+router.route("/all").get(ayahs);
+router.route("/:id").get(ayahById);
+router.route("/get-ayahs-by-surah/:id").get(getAyahsBySurah);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,16 +1,13 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
+
 const connectDB = require("./config/db");
+
 dotenv.config({ path: "./config/config.env" });
-// const fileUpload = require("express-fileupload");
-// const path = require("path");
-// const Connection = require("./Sockets/connection");
 
 const SurahRoutes = require("./routes/surah");
 const AyahRoutes = require("./routes/ayah");
-
-
 
 connectDB();
 
@@ -18,22 +15,13 @@ const app = express();
 const socket_io = require("socket.io");
 app.use(cors());
 app.use(express.json({ limit: "50mb" }));
-// app.use(express.static(__dirname + "/uploads"));
-// app.use(fileUpload());
 
 // routes
-app.use("/api/surah",SurahRoutes);
-app.use("/api/ayah",AyahRoutes);
-
-
+app.use("/api/surah", SurahRoutes);
+app.use("/api/ayah", AyahRoutes);
 
 var PORT = process.env.PORT || 5000;
 const io = socket_io();
-// Connection(app, io);
-// Lead();
-// AssignLead(io);
-// DailyMidNightTask();
-// Notification();
 
 io.listen(
   app.listen(PORT, console.log(`server is running on the port is ${PORT}`))
