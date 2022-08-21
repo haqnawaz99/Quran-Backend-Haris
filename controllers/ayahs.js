bf52986ba6546218809c5daddd6ab01f92a118d9
Haris Khalid <hariskhalid@folio3.com>
2022-08-21 15:17:08 +0500

    updated changes

--- controllers/ayahs.js
+++ controllers/ayahs.js
@@ -33,7 +33,6 @@ exports.ayahById = async (req, res) => {
     return res.status(500).json({ msg: "Server Error" });
   }
 };
-
 exports.getAyahsBySurah = async (req, res) => {
   try {
     const total = await Ayah.countDocuments({ SuraID: req.params.id });
@@ -70,6 +69,59 @@ exports.addNotestoAyah = async (req, res) => {
       },
     });
     return res.status(200).json({ msg: "Notes Added" });
+  } catch (err) {
+    console.log(err);
+    if (err.kind === "ObjectId")
+      return res.status(400).json({ msg: "invalid id format" });
+    return res.status(500).json({ msg: "Server Error" });
+  }
+};
+exports.deleteNotestoAyah = async (req, res) => {
+  try {
+    const errors = validationResult(req);
+    if (!errors.isEmpty())
+      return res.status(400).json({ errors: errors.array() });
+    let ayah = await Ayah.findOne({ _id: req.params.id });
+    if (!ayah)
+      return res.status(404).json({
+        msg: "no Ayah exists against user id: " + req.params.id,
+      });
+    await ayah.updateOne({
+      $pull: {
+        notes: { _id: req.body.noteId },
+      },
+    });
+    return res.status(200).json({ msg: "Note Deleted" });
+  } catch (err) {
+    console.log(err);
+    if (err.kind === "ObjectId")
+      return res.status(400).json({ msg: "invalid id format" });
+    return res.status(500).json({ msg: "Server Error" });
+  }
+};
+exports.updateNotestoAyah = async (req, res) => {
+  try {
+    const errors = validationResult(req);
+    if (!errors.isEmpty())
+      return res.status(400).json({ errors: errors.array() });
+    let ayah = await Ayah.findOne({ _id: req.params.id });
+    if (!ayah)
+      return res.status(404).json({
+        msg: "no Ayah exists against user id: " + req.params.id,
+      });
+
+    await Ayah.updateOne(
+      { _id: req.params.id, "notes._id": req.body.noteId },
+      { $set: { "notes.$.description": req.body.description } }
+    )
+      .then(() => {
+        return res.status(200).json({ msg: "note updated successfully" });
+      })
+      .catch(() => {
+        return res
+          .status(400)
+          .json({ msg: "error occured while updating ayah" });
+      });
   } catch (err) {
     console.log(err);
     if (err.kind === "ObjectId")
--- routes/ayah.js
+++ routes/ayah.js
@@ -1,18 +1,42 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 const { check } = require("express-validator");
 const auth = require("../middlewares/auth");
-const { ayahs, ayahById ,getAyahsBySurah,addNotestoAyah} = require('../controllers/ayahs');
+const {
+  ayahs,
+  ayahById,
+  getAyahsBySurah,
+  addNotestoAyah,
+  deleteNotestoAyah,
+  updateNotestoAyah
+} = require("../controllers/ayahs");
 
-router.route('/all').get(ayahs);
-router.route('/:id').get(ayahById);
+router.route("/all").get(ayahs);
+router.route("/:id").get(ayahById);
 
-router.route('/get-ayahs-by-surah/:id').get(getAyahsBySurah);
+router.route("/get-ayahs-by-surah/:id").get(getAyahsBySurah);
 
-router.route("/add-note/:id").put(
+router
+  .route("/add-note/:id")
+  .put(
     check("description", "Please Add valid note").not().isEmpty(),
     auth,
     addNotestoAyah
+  );
+router
+  .route("/delete-note/:id")
+  .delete(
+    check("noteId", "Please Add valid noteId").not().isEmpty(),
+    auth,
+    deleteNotestoAyah
+  );
+  router
+  .route("/update-note/:id")
+  .put(
+    check("noteId", "Please Add valid noteId").not().isEmpty(),
+    check("description", "Please Add valid description").not().isEmpty(),
+    auth,
+    updateNotestoAyah
   );
 
 module.exports = router;
